53ab0b877c0c94b24198588fbb11fdc5142ab8bf
Yonatan <yonatan@everthere.co>
2021-01-30 16:43:24 +0200

    Remove console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascript file is loaded!");
-
 const fetchWeather = (address, callback) => {
   fetch(`http://localhost:3000/weather?address=${address}`)
     .then((response) => {
