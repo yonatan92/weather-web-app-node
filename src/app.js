8606510fe26ba85200ac65a1270de2467aca5f66
Yonatan <yonatan@everthere.co>
2021-01-30 22:27:21 +0200

    Update Footer

--- src/app.js
+++ src/app.js
@@ -22,14 +22,14 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather",
-    name: "Andrew Mead",
+    name: "Yonathan Kurin",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Me",
-    name: "Andrew Mead",
+    name: "Yonathan Kurin",
   });
 });
 
@@ -37,7 +37,7 @@ app.get("/help", (req, res) => {
   res.render("help", {
     helpText: "This is some helpful text.",
     title: "Help",
-    name: "Andrew Mead",
+    name: "Yonathan Kurin",
   });
 });
 
